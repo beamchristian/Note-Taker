5135fc02efa41120c7adf726492f64d1dd995190
beamchristian <beamchristian@yahoo.com>
2021-11-02 18:33:38 -0400

    find notes in json data by id

--- server.js
+++ server.js
@@ -6,9 +6,23 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+function findById(id, notesArray) {
+  const result = notesArray.filter(note => note.id === id)[0];
+  return result;
+}
+
 app.get('/api/db', (req, res) => {
   let results = notes;
   res.json(results);
+});
+
+app.get('/api/db/:id', (req, res) => {
+  const result = findById(req.params.id, notes);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
 });
 
 app.listen(PORT, () => {
