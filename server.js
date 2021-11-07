a2451da3bfa58163a61de3176704d72399daf9db
beamchristian <beamchristian@yahoo.com>
2021-11-07 15:41:28 -0500

    creating note data and saving in db.json

--- server.js
+++ server.js
@@ -1,14 +1,41 @@
 const express = require('express');
-
 const { notes } = require('./db/db');
-
+const fs = require('fs');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 function findById(id, notesArray) {
   const result = notesArray.filter(note => note.id === id)[0];
   return result;
+}
+
+function createNewNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+  return note;
+}
+
+function validateNote(note) {
+  if (!note.name || typeof note.name !== 'string') {
+    return false;
+  }
+  if (!note.title || typeof note.title !== 'string') {
+    return false;
+  }
+  if (!note.text || typeof note.text !== 'string') {
+    return false;
+  }
+  return true;
 }
 // show all notes in json data
 app.get('/api/db', (req, res) => {
@@ -23,6 +50,19 @@ app.get('/api/db/:id', (req, res) => {
     res.json(result);
   } else {
     res.send(404);
+  }
+});
+
+app.post('/api/db', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = notes.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateNote(req.body)) {
+    res.status(400).send('The note is not properly formatted.');
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
   }
 });
 
