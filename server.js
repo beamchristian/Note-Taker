27ff3d9fa4a703239de7b9f11b6a59cb08a37355
beamchristian <beamchristian@yahoo.com>
2021-11-07 14:45:31 -0500

    findById

--- server.js
+++ server.js
@@ -10,12 +10,13 @@ function findById(id, notesArray) {
   const result = notesArray.filter(note => note.id === id)[0];
   return result;
 }
-
+// show all notes in json data
 app.get('/api/db', (req, res) => {
   let results = notes;
   res.json(results);
 });
 
+// show notes by particular id in json data else show 404 error
 app.get('/api/db/:id', (req, res) => {
   const result = findById(req.params.id, notes);
   if (result) {
